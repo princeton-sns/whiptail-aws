383a9fa3bfcaf0e3cf770a8a4d55b833f9ddb607
lisirrx <lisirrx@gmail.com>
2025-03-18 13:59:26 -0400

    Initial commit

---
+++ test/whiptil-aws.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as WhiptilAws from '../lib/whiptil-aws-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/whiptil-aws-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new WhiptilAws.WhiptilAwsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
